037d697ecedebf100681b26896af83f835bfe3ad
Zoe Lanz <zoe_lanz@hotmail.com>
2022-07-09 21:57:51 -0300

    media queries contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,27 +5,23 @@ import { Button } from "react-bootstrap";
 import "./Contact.scss";
 
 function Contact() {
-    return (
-        <div>
-
-        
+  return (
+    <div>
       <div className="contactContainer">
-
-      <div className="networkContainer">
-        <h5 className="inquires">For inquiries, questions, comments, etc. contact:</h5>
-        <div className="network">
-          <p>Galerie Balice Hertling</p>
-          <p>gallery@balicehertling.com</p>
-          <p>​+33 (0) 9 79 42 96 99</p>
+        <div className="networkContainer">
+          <h5 className="inquires">
+            For inquiries, questions, comments, etc. contact:
+          </h5>
+          <div className="network">
+            <p>Galerie Balice Hertling</p>
+            <p>gallery@balicehertling.com</p>
+            <p>​+33 (0) 9 79 42 96 99</p>
+          </div>
+          <div className="network">
+            <p>Mercedes Llanos</p>
+            <p>merchallanos@gmail.com</p>
+          </div>
         </div>
-
-        <div className="network">
-          <p>Mercedes Llanos</p>
-          <p>merchallanos@gmail.com</p>
-        </div>
-      </div>
-
-
         <form className="form" action="">
           <div className="row">
             <div className="col-100">
@@ -46,7 +42,6 @@ function Contact() {
                 className="w-100 input"
               />
             </div>
-
             <div className="col-75">
               <input
                 required
@@ -56,7 +51,6 @@ function Contact() {
                 className="w-100 input"
               />
             </div>
-
             <div className="col-75">
               <textarea
                 required
@@ -68,24 +62,12 @@ function Contact() {
                 rows="4"
               />
             </div>
-
             <div className="containerButton">
-              <Button 
-              //   onClick={(e) => {
-              //     purchaseOrder(e)
-              //   }}
-                className="button"
-              //   variant="secondary"
-              >
-                Submit
-              </Button>
+              <Button className="button">Submit</Button>
             </div>
           </div>
         </form>
-
-        
       </div>
-   
     </div>
   );
 }
